45fc9bd20dbef66eb682ee06429874171f7ec88b
Bukinich-Pavel <bukinich98@mail.ru>
2021-10-29 14:37:51 +0300

    WebApi

---
+++ WebApi/Controllers/CardsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebApi.Data;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CardsController : Controller
+    {
+        // GET api/cards
+        [HttpGet]
+        public IEnumerable<Card> Get()
+        {
+            return Repository.GetCards();
+        }
+
+
+        // POST api/cards
+        [HttpPost]
+        public void Post([FromBody] Card value)
+        {
+            Repository.AddCard(value);
+        }
+
+
+        // PUT api/cards/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Card value)
+        {
+            Repository.PutCard(id, value);
+        }
+
+
+        // DELETE api/cards/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            Repository.DeleteCard(id);
+        }
+
+
+    }
+}
---
+++ WebApi/Data/Repository.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Models;
+
+namespace WebApi.Data
+{
+    public class Repository
+    {
+        static List<Card> cards;
+
+
+        static Repository()
+        {
+            string str = File.ReadAllText("Cards.json");
+            cards = JsonConvert.DeserializeObject<List<Card>>(str);
+
+
+            //string json = JsonConvert.SerializeObject(cards);
+            //File.WriteAllText("Cards.json", json);
+
+        }
+
+
+        public static IEnumerable<Card> GetCards() => cards;
+
+        public static void AddCard(Card card)
+        {
+            cards.Add(card);
+
+            string json = JsonConvert.SerializeObject(cards);
+            File.WriteAllText("Cards.json", json);
+
+        }
+
+        public static void DeleteCard(int id)
+        {
+            cards.RemoveAt(id);
+
+            string json = JsonConvert.SerializeObject(cards);
+            File.WriteAllText("Cards.json", json);
+        }
+
+        public static void PutCard(int id, Card card)
+        {
+            cards.RemoveAt(id);
+            cards.Insert(id, card);
+
+            string json = JsonConvert.SerializeObject(cards);
+            File.WriteAllText("Cards.json", json);
+        }
+
+
+    }
+}
